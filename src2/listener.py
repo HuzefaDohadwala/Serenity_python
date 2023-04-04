c6cbd9d22f98286bdab0b1db4faee9767abc8322
Karbelekedar <karbelekedar84@gmail.com>
2023-04-04 19:51:15 +0530

    Button disappar after accepting req

--- src2/listener.py
+++ src2/listener.py
@@ -4,6 +4,84 @@ import customtkinter
 
 import mysql.connector
 
+# class Listener_landing(customtkinter.CTkFrame):
+#     def __init__(self, master=None, username=None):
+#         super().__init__(master)
+#         self.master = master
+#         self.pack(fill='both', expand=True)
+#
+#         # Create the frame to hold the components
+#         self.inner_frame = customtkinter.CTkFrame(self)  # set background color of inner frame to black
+#         self.inner_frame.pack(fill='both', expand=True)
+#
+#         # Create the components
+#         self.welcome = customtkinter.CTkLabel(self.inner_frame, text=f"Welcome {username}!",
+#                                               font=('Century Gothic', 25))
+#         self.request_label = customtkinter.CTkLabel(self.inner_frame, text="Requests:", font=('Century Gothic', 15))
+#         self.request_frame = customtkinter.CTkFrame(self.inner_frame)
+#         self.accept_buttons = []
+#
+#         # Place the components
+#         self.welcome.pack(side='top', padx=20, pady=50)
+#         self.request_label.pack(side='top', padx=20, pady=10)
+#         self.request_frame.pack(side='top', padx=20, pady=10)
+#
+#         # Fetch requests initially
+#         self.fetch_requests()
+#
+#         # Set up a timer to fetch requests every 5 seconds
+#         self.master.after(5000, self.fetch_requests)
+#
+#     def accept_request(self, request_id):
+#         # Remove the request from the database
+#         db = mysql.connector.connect(host='localhost', user='root', password='Kedar@2004', database='serenity')
+#         cursor = db.cursor()
+#         cursor.execute("DELETE FROM requests WHERE id=%s", (request_id,))
+#         db.commit()
+#
+#         # Clear the request frame and fetch requests again
+#         for button in self.accept_buttons:
+#             button.destroy()
+#         self.accept_buttons = []
+#         self.fetch_requests()
+#
+#     def fetch_requests(self):
+#         # Connect to the database
+#         db = mysql.connector.connect(host='localhost', user='root', password='Kedar@2004', database='serenity')
+#
+#         # Create a cursor object
+#         cursor = db.cursor()
+#
+#         # Execute the SQL query to fetch all requests
+#         cursor.execute("SELECT * FROM requests")
+#
+#         # Fetch all the rows and store them in a list
+#         rows = cursor.fetchall()
+#
+#         # Clear the existing requests
+#         for button in self.accept_buttons:
+#             button.destroy()
+#         self.accept_buttons = []
+#
+#         # Add the new requests
+#         for row in rows:
+#             request_frame = customtkinter.CTkFrame(self.request_frame)
+#             request_frame.pack(side='top', padx=10, pady=10, fill='x')
+#
+#             username = row[1]
+#             request_message = row[2]
+#             request_label = customtkinter.CTkLabel(request_frame, text=f"{username}: {request_message}")
+#             request_label.pack(side='left', padx=10)
+#
+#             accept_button = customtkinter.CTkButton(request_frame, text="Accept",
+#                                                     font=('Century Gothic', 15),
+#                                                     command=lambda id=row[0]: self.accept_request(id))
+#             accept_button.pack(side='right', padx=10)
+#             self.accept_buttons.append(accept_button)
+#
+#         # Set up a timer to fetch requests every 5 seconds
+#         self.master.after(5000, self.fetch_requests)
+
 class Listener_landing(customtkinter.CTkFrame):
     def __init__(self, master=None, username=None):
         super().__init__(master)
@@ -18,62 +96,57 @@ class Listener_landing(customtkinter.CTkFrame):
         self.welcome = customtkinter.CTkLabel(self.inner_frame, text=f"Welcome {username}!",
                                               font=('Century Gothic', 25))
         self.request_label = customtkinter.CTkLabel(self.inner_frame, text="Requests:", font=('Century Gothic', 15))
-        self.request_list = tkinter.Listbox(self.inner_frame, height=10, width=50)
-        self.accept_button = customtkinter.CTkButton(self.inner_frame, text="Accept",
-                                                     font=('Century Gothic', 15), command=self.accept_request)
+        self.request_frame = customtkinter.CTkFrame(self.inner_frame)
+        self.accept_buttons = []
 
         # Place the components
         self.welcome.pack(side='top', padx=20, pady=50)
         self.request_label.pack(side='top', padx=20, pady=10)
-        self.request_list.pack(side='top', padx=20, pady=10)
-        self.accept_button.pack(side='top', padx=20, pady=10)
+        self.request_frame.pack(side='top', padx=20, pady=10)
 
         # Fetch requests initially
-        self.fetch_requests()
+        self.fetch_and_display_requests()
 
         # Set up a timer to fetch requests every 5 seconds
-        self.master.after(5000, self.fetch_requests)
+        self.master.after(5000, self.fetch_and_display_requests)
 
-    def accept_request(self):
-        # Get the selected item from the listbox
-        selection = self.request_list.curselection()
-        if len(selection) == 0:
-            return
-
-        # Get the request message from the selected item
-        request_message = self.request_list.get(selection[0])
-
+    def accept_request(self, request_id):
         # Remove the request from the database
         db = mysql.connector.connect(host='localhost', user='root', password='Kedar@2004', database='serenity')
         cursor = db.cursor()
-        cursor.execute("DELETE FROM requests WHERE message=%s", (request_message,))
+        cursor.execute("DELETE FROM requests WHERE id=%s", (request_id,))
         db.commit()
 
-        # Clear the listbox and fetch requests again
-        self.request_list.delete(0, tkinter.END)
-        self.fetch_requests()
+        # Clear the request frame and fetch requests again
+        for button in self.accept_buttons:
+            button.destroy()
+        self.accept_buttons = []
+        self.fetch_and_display_requests()
 
-    def fetch_requests(self):
-        # Connect to the database
+    def fetch_and_display_requests(self):
+        # Clear the request frame
+        for child in self.request_frame.winfo_children():
+            child.destroy()
+
+        # Connect to the database and fetch all requests
         db = mysql.connector.connect(host='localhost', user='root', password='Kedar@2004', database='serenity')
-
-        # Create a cursor object
         cursor = db.cursor()
-
-        # Execute the SQL query to fetch all requests
         cursor.execute("SELECT * FROM requests")
-
-        # Fetch all the rows and store them in a list
         rows = cursor.fetchall()
 
-        # Clear the existing list items
-        self.request_list.delete(0, tkinter.END)
-
-        # Add the new list items
+        # Display each request with an accept button
         for row in rows:
             username = row[1]
             request_message = row[2]
-            self.request_list.insert(tkinter.END, f"{username}: {request_message}")
 
-        # Set up a timer to fetch requests every 5 seconds
-        self.master.after(5000, self.fetch_requests)
+            request_frame = customtkinter.CTkFrame(self.request_frame)
+            request_label = customtkinter.CTkLabel(request_frame, text=f"{username}: {request_message}")
+            accept_button = customtkinter.CTkButton(request_frame, text="Accept", font=('Century Gothic', 15),
+                                                    command=lambda request_id=row[0]: self.accept_request(request_id))
+            request_label.pack(side='left')
+            accept_button.pack(side='left', padx=10)
+
+            request_frame.pack(side='top', padx=20, pady=5)
+            self.accept_buttons.append(accept_button)
+
+        db.close()
